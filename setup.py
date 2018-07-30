4b6fc375c048a539bc9c441d5a5a38e721df4fee
Luis Ulloa <timken@gmail.com>
2018-07-30 22:45:28 +0100

    Improving setup.py for dynamic executable

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ from setuptools import setup
 # TODO is there a better way ? dependencies seem to always require the version
 # Calling only at the egg_info step gives us the wanted depth first behavior
 if 'egg_info' in sys.argv and os.getenv('CAPE_DEPENDENCIES', 'False').lower() == 'true':
-    subprocess.check_call(['pip3', 'install','--no-warn-conflicts','--upgrade', '-r', 'requirements.txt'])
+    pip3_executable = os.path.join(os.path.abspath(os.path.join(os.path.dirname(sys.executable))), 'pip3')
+    if not os.path.isfile(pip3_executable):
+        pip3_executable = 'pip3'
+    subprocess.check_call([pip3_executable, 'install','--no-warn-conflicts','--upgrade', '-r', 'requirements.txt'])
 
 setup(
     name=NAME,
@@ -16,6 +19,7 @@ setup(
     author='Bloomsbury AI',
     author_email='contact@bloomsbury.ai',
     packages=PACKAGES,
+    package_dir={'docqa': 'document-qa/docqa'},
     include_package_data=True,
     package_data={
         '': ['*.*'],
