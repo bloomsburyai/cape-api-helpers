695205951a7860e73d5df98ab86d77ccb37fd17e
Luis Ulloa <timken@gmail.com>
2018-07-31 12:41:36 +0100

    removed unused imports

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-import os
-import sys
-import subprocess
 from package_settings import NAME, VERSION, PACKAGES, DESCRIPTION
 from setuptools import setup
-
-# # TODO is there a better way ? dependencies seem to always require the version
-# # Calling only at the egg_info step gives us the wanted depth first behavior
-# if 'egg_info' in sys.argv and os.getenv('CAPE_DEPENDENCIES', 'False').lower() == 'true':
-#     pip3_executable = os.path.join(os.path.abspath(os.path.join(os.path.dirname(sys.executable))), 'pip3')
-#     if not os.path.isfile(pip3_executable):
-#         pip3_executable = 'pip3'
-#     subprocess.check_call([pip3_executable, 'install','--no-warn-conflicts','--upgrade', '-r', 'requirements.txt'])
 
 setup(
     name=NAME,
@@ -22,9 +11,9 @@ setup(
     package_dir={'docqa': 'document-qa/docqa'},
     include_package_data=True,
     install_requires=[
-            'pytest==3.6.4',
-            'pydevd==1.1.1',
-        ],
+        'pytest==3.6.4',
+        'pydevd==1.1.1',
+    ],
     package_data={
         '': ['*.*'],
     },
